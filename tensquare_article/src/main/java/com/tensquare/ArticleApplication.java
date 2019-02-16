6b0bcd4855aeb72193636cbfc7e20ca82de9ec7f
lastLi <493734068@qq.com>
2019-02-16 22:45:55 +0800

    补充

---
+++ tensquare_article/src/main/java/com/tensquare/ArticleApplication.java
@@ -0,0 +1,27 @@
+package com.tensquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import util.IdWorker;
+import util.MyPageQuery;
+
+@CrossOrigin
+@SpringBootApplication
+public class ArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
+    }
+
+
+    @Bean
+    public MyPageQuery myPageQuery() {
+        return new MyPageQuery();
+    }
+}
