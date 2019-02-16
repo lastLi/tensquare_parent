2c3d4d1acaee674045f5c37d181b056d6c1fc28d
lastLi <493734068@qq.com>
2019-02-16 15:33:32 +0800

    修改了热门查询的逻辑

--- tensquare_base/src/main/java/com/tensquare/base/dao/LabelDao.java
+++ tensquare_base/src/main/java/com/tensquare/base/dao/LabelDao.java
@@ -11,18 +11,20 @@ import java.util.List;
 /**
  * 标签Dao
  */
-public interface LabelDao extends JpaRepository<Label,String>, JpaSpecificationExecutor<Label> {
+public interface LabelDao extends JpaRepository<Label, String>, JpaSpecificationExecutor<Label> {
     /**
      * Recommend的值是1就是推荐,0就是不推荐
-     * @return  推荐标签列表
+     * 并且状态还只能是不等于0的
+     * @return 推荐标签列表
      */
-    @Query(value = "SELECT * FROM tensquare_base.tb_label WHERE recommend=1",nativeQuery = true)
+    @Query(value = "SELECT * FROM tensquare_base.tb_label WHERE recommend=1 AND state!=0", nativeQuery = true)
     List<Label> recommendedTagList();
 
     /**
      * State的值是1就是有效,0就是无效.
+     *
      * @return 有效标签列表
      */
-    @Query(value = "SELECT * FROM tensquare_base.tb_label WHERE state=1",nativeQuery = true)
+    @Query(value = "SELECT * FROM tensquare_base.tb_label WHERE state=1", nativeQuery = true)
     List<Label> validTagList();
 }
